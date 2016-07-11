18e708276ca2d826701cdaa1567ad117b88bb274
Fei Yuan <yuan@nscl.msu.edu>
2016-07-11 11:11:50 -0400

    Allow num_samples to be controlled

--- src/bin/pi.rs
+++ src/bin/pi.rs
@@ -1,20 +1,30 @@
+#[macro_use]
+extern crate clap;
 extern crate rand;
 
+use clap::{Arg, App};
 use rand::distributions::{IndependentSample, Range};
 
 fn main() {
+    let matches = App::new("pi")
+        .arg(Arg::with_name("NUM_SAMPLES"))
+        .get_matches();
+
+    let num_samples =
+        value_t!(matches, "NUM_SAMPLES", u64)
+        .unwrap_or_else(|e| e.exit());
+
     let between = Range::new(-1.0f64, 1.0);
     let mut rng = rand::thread_rng();
 
-    println!("calculating pi ...");
-    let n = 10000000;
+    println!("calculating pi with {} samples ...", num_samples);
     let mut count = 0;
-    for _ in 0 .. n {
+    for _ in 0 .. num_samples {
         let x = between.ind_sample(&mut rng);
         let y = between.ind_sample(&mut rng);
         if x * x + y * y <= 1.0 {
             count += 1;
         }
     }
-    println!("pi = {}", 4. * (count as f64) / (n as f64));
+    println!("pi = {}", 4. * (count as f64) / (num_samples as f64));
 }
