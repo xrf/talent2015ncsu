30fdf3b106a9e2b2c1de57d99f6c6385e970301e
Fei Yuan <yuan@nscl.msu.edu>
2016-07-27 16:43:11 -0400

    Add energy statistics

--- src/bin/determinantalmc.rs
+++ src/bin/determinantalmc.rs
@@ -63,6 +63,14 @@ struct Args {
     flag_param_g: f64,
 }
 
+fn inverse(m: &ArrayView<f64, (usize, usize)>) -> Array<f64, (usize, usize)> {
+    let n = m.shape()[0];
+    assert_eq!(n, m.shape()[1]);
+    let m = ::la::Matrix::new(n, n, m.iter().cloned().collect());
+    Array::from_iter(m.inverse().unwrap().get_data().iter().cloned())
+        .into_shape((n, n)).unwrap()
+}
+
 fn log_det_sq(m: &ArrayView<f64, (usize, usize)>) -> f64 {
     let n = m.shape()[0];
     assert_eq!(n, m.shape()[1]);
@@ -88,46 +96,72 @@ fn kinetic_matrix(nx: usize) -> Array<f64, (usize, usize)> {
 }
 
 fn apply_potential(sqrt_c: f64,
+                   d_sqrt_c: f64,
                    transfer: &mut ArrayViewMut<f64, (usize, usize)>,
+                   d_transfer: &mut ArrayViewMut<f64, (usize, usize)>,
                    sigma: &ArrayView<i8, usize>) {
     let nx = transfer.shape()[0];
     for x in 0 .. nx {
         for i in 0 .. nx {
-            transfer[(x, i)] *= 1.0 + sqrt_c * sigma[x] as f64;
+            let sigma_x = sigma[x] as f64;
+            let b = 1.0 + sqrt_c * sigma_x;
+            let transfer_x_i = transfer[(x, i)];
+            transfer[(x, i)] = b * transfer_x_i;
+            d_transfer[(x, i)] =
+                d_sqrt_c * sigma_x * transfer_x_i +
+                b * d_transfer[(x, i)];
         }
     }
 }
 
 struct PartitionFunction {
     sqrt_c: f64,
+    d_sqrt_c: f64,
     evolve_kinetic: Array<f64, (usize, usize)>,
+    d_evolve_kinetic: Array<f64, (usize, usize)>,
 }
 
 impl PartitionFunction {
     fn new(nx: usize, dt: f64, mdx2: f64, g: f64) -> PartitionFunction {
+        let exp_dt_g = (dt * g).exp();
+        let t = kinetic_matrix(nx);
         PartitionFunction {
-            sqrt_c: (dt * g).exp() - 1.0,
-            evolve_kinetic:  {
-                let mut t = kinetic_matrix(nx);
-                t *= 0.25 * dt / mdx2;
-                Array::eye(nx) - t
-            },
+            sqrt_c: exp_dt_g - 1.0,
+            d_sqrt_c: exp_dt_g * g,
+            evolve_kinetic: Array::eye(nx) + (-0.25 * dt / mdx2) * &t,
+            d_evolve_kinetic: (-0.25 / mdx2) * &t,
         }
     }
 
-    fn eval(&self, sigma: &ArrayView<i8, (usize, usize)>) -> f64 {
+    fn eval(&self, sigma: &ArrayView<i8, (usize, usize)>) -> (f64, f64) {
         let nt = sigma.shape()[0];
         let nx = sigma.shape()[1];
         let mut transfer = Array::eye(nx);
+        let mut d_transfer = Array::zeros((nx, nx));
         for t in 0 .. nt {
+            d_transfer =
+                self.d_evolve_kinetic.dot(&transfer) +
+                self.evolve_kinetic.dot(&d_transfer);
             transfer = self.evolve_kinetic.dot(&transfer);
             apply_potential(self.sqrt_c,
+                            self.d_sqrt_c,
                             &mut transfer.view_mut(),
+                            &mut d_transfer.view_mut(),
                             &sigma.subview(Axis(0), t));
+            d_transfer =
+                self.d_evolve_kinetic.dot(&transfer) +
+                self.evolve_kinetic.dot(&d_transfer);
             transfer = self.evolve_kinetic.dot(&transfer);
         }
         let fermi = Array::eye(nx) + transfer;
-        log_det_sq(&fermi.view())
+        let d_fermi = d_transfer;
+        let mdm = 2.0 * &inverse(&fermi.view()).dot(
+            &((-1.0 / (nt as f64)) * d_fermi));
+        let mut tr_mdm = 0.0;
+        for i in 0 .. nx {
+            tr_mdm = mdm[(i, i)];
+        }
+        (log_det_sq(&fermi.view()), tr_mdm)
     }
 }
 
@@ -157,7 +191,8 @@ fn main() {
     let partition_function = PartitionFunction::new(nx, dt, mdx2, g);
 
     let mut sigma = Array::from_elem((nt, nx), 1i8);
-    let mut z = partition_function.eval(&sigma.view());
+    let (mut z, mut e) = partition_function.eval(&sigma.view());
+    let mut e_sum = 0.0;
     for _ in 0 .. num_samples {
 
         for _ in 0 .. sampling_interval {
@@ -165,22 +200,26 @@ fn main() {
             let t_change = t_range.ind_sample(&mut rng);
             sigma[(t_change, x_change)] *= -1;
 
-            let new_z = partition_function.eval(&sigma.view());
+            let (new_z, new_e) = partition_function.eval(&sigma.view());
             println!("# {} -> {}", z, new_z);
+            println!("# {} -> {}", e, new_e);
 
             if rng.next_f64() < (new_z - z).exp() {
-                z = new_z
+                z = new_z;
+                e = new_e;
             } else {
                 println!("# rejected");
                 sigma[(t_change, x_change)] *= -1;
             }
         }
 
-        // TODO: compute statistics
+        // compute statistics (currently just energy)
+        e_sum += e;
 
     }
 
     println!("{{");
     println!("'num_samples': {:?},", num_samples);
+    println!("'energy_avg': {:?},", e_sum / (num_samples as f64));
     println!("}}");
 }
